1bda454a71615702080f985abb88a7b1223f4ae8
kozmarti <kozmarti@gmail.com>
2024-09-18 22:25:59 +0200

    connect strava api

--- pages/activities.tsx
+++ pages/activities.tsx
@@ -1,5 +1,16 @@
 import { getActivities } from "@/lib/strava";
 
+const Activities: React.FC<any> = ({ activities }) => {
+  return (
+      <div>
+          {activities}
+      </div>
+  );
+};
+
+
+export default Activities;
+
 export const getStaticProps = async () => {
   const activities = await getActivities();
   return {
