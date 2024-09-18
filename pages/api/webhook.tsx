45d5d8394024137e92377460ddbc4de0d5d0a1b1
kozmarti <kozmarti@gmail.com>
2024-09-18 23:47:31 +0200

    test

--- pages/api/webhook.tsx
+++ pages/api/webhook.tsx
@@ -6,27 +6,32 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     const data = req.body
     if(data["aspect_type"] =="update"){
       console.log("YEEEES")
+
     }
     res.status(200).send("EVENT_RECEIVED");
    }
-  // console.log("HEREEE");
-  // // Your verify token. Should be a random string.
-  // const VERIFY_TOKEN = "STRAVA";
-  // // Parses the query params
-  // let mode = req.query["hub.mode"];
-  // let token = req.query["hub.verify_token"];
-  // let challenge = req.query["hub.challenge"];
-  // // Checks if a token and mode is in the query string of the request
-  // if (mode && token) {
-  //   // Verifies that the mode and token sent are valid
-  //   if (mode === "subscribe" && token === VERIFY_TOKEN) {
-  //     // Responds with the challenge token from the request
-  //     console.log("WEBHOOK_VERIFIED");
-  //     console.log({ "hub.challenge": challenge });
-  //     res.json({ "hub.challenge": challenge });
-  //   } else {
-  //     // Responds with '403 Forbidden' if verify tokens do not match
-  //     res.status(403);
-  //   }
-  // }
+  console.log("HEREEE");
+  // Your verify token. Should be a random string.
+  const VERIFY_TOKEN = "STRAVA";
+  // Parses the query params
+  let mode = req.query["hub.mode"];
+  let token = req.query["hub.verify_token"];
+  let challenge = req.query["hub.challenge"];
+  // Checks if a token and mode is in the query string of the request
+  console.log(mode);
+  console.log(token);
+  console.log(challenge);
+
+  if (mode && token) {
+    // Verifies that the mode and token sent are valid
+    if (mode === "subscribe" && token === VERIFY_TOKEN) {
+      // Responds with the challenge token from the request
+      console.log("WEBHOOK_VERIFIED");
+      console.log({ "hub.challenge": challenge });
+      res.json({ "hub.challenge": challenge });
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.status(403);
+    }
+  }
 };
