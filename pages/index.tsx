3b6d23fdc983809f6048e28204999f7fc1083ea8
Marta Kozma <kozmarti@gmail.com>
2025-04-17 19:05:15 +0200

    make it client

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { Fredoka } from 'next/font/google';
-import client from "@/lib/mongodb";
+import { useEffect, useState } from "react";
 import type { InferGetServerSidePropsType, GetServerSideProps } from "next";
 import ElevationChart from "@/app/components/ElevationChart";
 import { MapComponent } from "@/app/components/MapComponent";
@@ -24,39 +24,55 @@ type Stats = {
   altitude_by_day: number[][];
   distance_aggregated: number[];
   delta_distances: number[];
-  delta_altitudes: number[];  
-  // Add other fields as needed
+  delta_altitudes: number[];
 };
 
 const inter = Fredoka({ subsets: ["latin"] });
 
 
-export const getServerSideProps: GetServerSideProps<{ stats: Stats | null }> = async () => {
-  const apiUrl = process.env.NEXT_PUBLIC_API_URL;
-  const userId = process.env.STRAVA_USER_ID;
+export default function Home() {
+  const [stats, setStats] = useState<Stats | null>(null);
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    const fetchStats = async () => {
+      const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+      const userId = process.env.STRAVA_USER_ID;
 
-  try {
-    const res = await fetch(`${apiUrl}/api/user/${userId}/project/test/stats`);
-    const data = await res.json();
-
-    return {
-      props: {
-        stats: data,
-      },
+      try {
+        const res = await fetch(`${apiUrl}/api/user/${userId}/project/test/stats`);
+        const data = await res.json();
+        setStats(data);
+      } catch (error) {
+        console.error("Failed to fetch stats:", error);
+      } finally {
+        setLoading(false);
+      }
     };
-  } catch (error) {
-    console.error("Failed to fetch stats:", error);
-    return {
-      props: {
-        stats: null,
-      },
-    };
+
+    fetchStats();
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="loader">
+        <Image
+          src="/logo.png"
+          alt="Hike&Donate Logo"
+          width={200}
+          height={48}
+          priority
+        />
+        {/* Show loading spinner or skeleton screen while data is loading */}
+        <p>Loading...</p>
+      </div>
+    );
   }
-};
+  if (!stats) {
+    return <p>Failed to load stats</p>;
+  }
 
-export default function Home({ stats }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  if (!stats) return <p>Failed to load stats</p>;
+
   const coords = stats.coordinates;
   const lastCords = stats.coordinates.slice(-1)[0];
 
@@ -66,7 +82,7 @@ export default function Home({ stats }: InferGetServerSidePropsType<typeof getSe
       className={`flex min-h-screen flex-col items-center justify-between p-24 global-background ${inter.className}`}
     >
 
-       <Image
+      <Image
         src="/logo.png"
         alt="Hike&Donate Logo"
         width={200}
@@ -81,21 +97,21 @@ Whether I walk 10 kilometers or 100, every euro raised will go toward transformi
 </p>
 <p>How far do you think I will make it ?</p>
 */}
-<div className="container wrapper">
+      <div className="container wrapper">
 
-      <PerformanceItemComponent title="totalDistance" quantity={stats.totalDistance / 1000}/>
-      <PerformanceItemComponent title="timeElapsed" quantity={stats.timeElapsed + 1}/>
+        <PerformanceItemComponent title="totalDistance" quantity={stats.totalDistance / 1000} />
+        <PerformanceItemComponent title="timeElapsed" quantity={stats.timeElapsed + 1} />
 
-      <PerformanceItemComponent title="totalElevationGain" quantity={stats.totalElevationGain}/>
-      <PerformanceItemComponent title="totalElevationLoss" quantity={stats.totalElevationLoss}/>
-      <PerformanceItemComponent title="maxAltitude" quantity={stats.maxAltitude}/>
+        <PerformanceItemComponent title="totalElevationGain" quantity={stats.totalElevationGain} />
+        <PerformanceItemComponent title="totalElevationLoss" quantity={stats.totalElevationLoss} />
+        <PerformanceItemComponent title="maxAltitude" quantity={stats.maxAltitude} />
 
-      <PerformanceItemComponent title="minAltitude" quantity={stats.minAltitude}/>
+        <PerformanceItemComponent title="minAltitude" quantity={stats.minAltitude} />
 
       </div>
-      <ElevationChart altitude={stats.altitudes} distance={stats.distance_aggregated}/>
+      <ElevationChart altitude={stats.altitudes} distance={stats.distance_aggregated} />
 
-      <MapComponent coordinates={coords as [number, number][]} currentLocation={lastCords as [number, number]} centerCoordinates={lastCords as [number, number]}/> 
+      <MapComponent coordinates={coords as [number, number][]} currentLocation={lastCords as [number, number]} centerCoordinates={lastCords as [number, number]} />
 
     </main>
   );
