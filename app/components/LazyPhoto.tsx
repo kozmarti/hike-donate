a1a39e8a1af563d583c115dc6fe720e2181500c3
Marta Kozma <kozmarti@gmail.com>
2025-07-08 14:12:45 +0200

    photo format

--- app/components/LazyPhoto.tsx
+++ app/components/LazyPhoto.tsx
@@ -2,33 +2,33 @@ import { useState } from 'react';
 import { useInView } from 'react-intersection-observer';
 
 const LazyPhoto = ({ src, date, ...props }: { src: string; date?: string }) => {
-    const [inViewRef, inView] = useInView({ triggerOnce: true });
-    const [loaded, setLoaded] = useState(false);
-  
-    return (
-        <>
-      <div ref={inViewRef} style={{ position: "relative", padding: "20px",backgroundColor: "white", borderRadius: "4px", boxShadow: "0px 3px 3px -2px rgb(0 0 0 / 20%), 0px 3px 4px 0px rgb(0 0 0 / 14%), 0px 1px 8px 0px rgb(0 0 0 / 12%)", 
-      transition: "opacity 1s ease-in-out", }}>
+  const [inViewRef, inView] = useInView({ triggerOnce: true });
+  const [loaded, setLoaded] = useState(false);
+
+  return (
+      <div ref={inViewRef}>
         {inView && (
-          <img
-            {...props}
-            src={src}
-            alt={"photo of" + date}
-            loading="lazy"
-            style={{
-            // @ts-ignore
-              ...props.style,
-              opacity: loaded ? 1 : 0,
-              transition: "opacity 1s ease-in-out",
-            }}
-            onLoad={() => setLoaded(true)}
-          />
+          <>
+              <img
+                {...props}
+                src={src}
+                alt={"photo of" + date}
+                loading="lazy"
+                style={{
+                  // @ts-ignore
+                  ...props.style,
+                  opacity: loaded ? 1 : 0,
+                  transition: "opacity 1s ease-in-out",
+                  padding: "20px", paddingBottom: "40px", backgroundColor: "white", borderRadius: "4px", boxShadow: "0px 3px 3px -2px rgb(0 0 0 / 20%), 0px 3px 4px 0px rgb(0 0 0 / 14%), 0px 1px 8px 0px rgb(0 0 0 / 12%)",
+              
+                }}
+                onLoad={() => setLoaded(true)}
+              />
+            <div style={{position: "relative", top: "-30px", right: "20px"}} className={`photo-date`}>{date}</div>
+          </>
         )}
-              <div className={`photo-date`}>{date}</div>
       </div>
+  );
+};
 
-      </>
-    );
-  };
-
-  export default LazyPhoto;
+export default LazyPhoto;
