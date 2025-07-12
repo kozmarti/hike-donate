a745e9bfef1e71091e8952f1688c0e0a1282388b
Marta Kozma <kozmarti@gmail.com>
2025-07-12 12:28:10 +0200

    opengraph metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,9 @@ export const metadata: Metadata = {
     description: "Track my fundraising hike where every kilometer helps raise donations for Ukraine. Follow the journey, view progress, and support the cause.",
     images: [
       {
-        url: '/bg-1.jpg',
+        url: '/review.png',
+        width: 1200,
+        height: 630,
         alt: 'Hike&Donate Fundraising Preview',
       },
     ],
