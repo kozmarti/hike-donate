a415c53beb71f8d8962907d296253a3eda70cabf
Marta Kozma <kozmarti@gmail.com>
2025-07-12 09:40:58 +0200

    transârent photo padding

--- app/components/LazyPhoto.tsx
+++ app/components/LazyPhoto.tsx
@@ -19,7 +19,7 @@ const LazyPhoto = ({ src, date, ...props }: { src: string; date?: string }) => {
                   ...props.style,
                   opacity: loaded ? 1 : 0,
                   transition: "opacity 1s ease-in-out",
-                  padding: "20px", paddingBottom: "40px", backgroundColor: "white", borderRadius: "4px", boxShadow: "0px 3px 3px -2px rgb(0 0 0 / 20%), 0px 3px 4px 0px rgb(0 0 0 / 14%), 0px 1px 8px 0px rgb(0 0 0 / 12%)",
+                  padding: "20px", paddingBottom: "40px", backgroundColor: "rgba(255, 255, 255, 0.5)", borderRadius: "4px", boxShadow: "0px 3px 3px -2px rgb(0 0 0 / 20%), 0px 3px 4px 0px rgb(0 0 0 / 14%), 0px 1px 8px 0px rgb(0 0 0 / 12%)",
               
                 }}
                 onLoad={() => setLoaded(true)}
