5a88549ac3d1d17ed48729cba822ffafa7589dcb
Marta Kozma <kozmarti@gmail.com>
2025-07-12 18:03:08 +0200

    reverse chronological order in photo album

--- app/api/user/[strava_user_id]/project/[project_slug]/stats/route.tsx
+++ app/api/user/[strava_user_id]/project/[project_slug]/stats/route.tsx
@@ -86,6 +86,7 @@ export async function GET(
                 in: { $concatArrays: ["$$value", "$$this"] },
               },
             },
+            photosUrl: { $reverseArray: "$photosUrl" }
           },
         },
         {
