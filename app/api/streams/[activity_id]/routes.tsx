77628ee4d17231afd4b12dbad2de19086aa24842
Marta Kozma <kozmarti@gmail.com>
2025-09-22 18:12:31 +0200

    add users credentials to call strava api

--- app/api/streams/[activity_id]/routes.tsx
+++ app/api/streams/[activity_id]/routes.tsx
@@ -6,8 +6,9 @@ export async function GET(
   { params }: { params: { activity_id: number } }
 ) {
   const { activity_id } = params;
+  
   console.log(params);
-  const streams = await getActivityStreams(Number(activity_id));
-  console.log(streams);
-  return NextResponse.json(streams);
+  //const streams = await getActivityStreams(Number(activity_id));
+  //console.log(streams);
+  return NextResponse.json("streams");
 }
